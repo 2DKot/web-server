e9bd8e108abd5ff1ec6003be310ffdda1904d333
2DKot <baidinkn@ya.ru>
2016-08-14 16:32:06 +0300

    fix: import module with mongoose connection

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+require('./model/mongoose_connection')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
